64fa450288069ab9fc755ab6f1b2cadb9a4e5efe
Nicole <nk845@georgetown.edu>
2021-02-11 16:23:55 -0500

    breakeven point

--- code/Wk4_CaseStudy.R
+++ code/Wk4_CaseStudy.R
@@ -27,11 +27,25 @@ MoE <- t_alpha * SEM
 lower <- round(x_bar - MoE, 4)
 upper <- round(x_bar + MoE, 4)
 
-round(MoE, 4)
 lower
 upper
 # 95% CI for number of cavities treated per dentist per week:
 # 3.843-4.187
+
+mean_CI <- function(x_bar, sigma, n, alpha){
+  if(n >= 50) {
+    SEM <- sigma/sqrt(n)
+    t_alpha <- qt(1-alpha/2, df = n-1)
+    MoE <- t_alpha*SEM
+    cat("Lower limit of mean CI is", toString(round(x_bar - MoE, 2)), "\n")
+    cat("Upper limit of mean CI is", toString(round(x_bar + MoE, 2)), "\n")
+    cat(toString(x_bar), "+/-", toString(round(MoE, 2)), "\n")
+  } else {
+    print("Sample size not large enough to assume Normality")
+  }
+}
+
+mean_CI(x_bar, sigma, n, alpha)
 
 
 
@@ -53,30 +67,34 @@ fixed_costs <- 4000000
 # Unit costs & profits not dependent on number of cavities filled per year
 unit_costs <- 200*dentists_using
 unit_sales <- 200*dentists_using
-unit_profits <- 0*dentists_using
 
 # Solution costs & profits for lower bound of CI
 solution_costs_lower <- 0.5*dentists_using*52*lower
 solution_sales_lower <- 2.5*dentists_using*52*lower
-solution_profits_lower <- 2*dentists_using*52*lower
 
 # Solution costs & profits for upper bound of CI
 solution_costs_upper <- 0.5*dentists_using*52*upper
 solution_sales_upper <- 2.5*dentists_using*52*upper
-solution_profits_upper <- 2*dentists_using*52*upper
 
 
 # ROI for lower bound of CI
-total_costs_lower <- unit_costs + solution_costs_lower + fixed_costs
-total_sales_lower <- (unit_sales + solution_sales_lower)
+total_costs_lower <- fixed_costs + unit_costs + solution_costs_lower
+total_sales_lower <- unit_sales + solution_sales_lower
 ROI_lower <- (total_sales_lower - total_costs_lower)/total_costs_lower * 100
 
 # ROI for upper bound of CI
-total_costs_upper <- unit_costs + solution_costs_upper + fixed_costs
-total_sales_upper <- (unit_sales + solution_sales_upper)
+total_costs_upper <- fixed_costs + unit_costs + solution_costs_upper
+total_sales_upper <- unit_sales + solution_sales_upper
 ROI_upper <- (total_sales_upper - total_costs_upper)/total_costs_upper * 100
 
 
 round(ROI_lower, 2)
 round(ROI_upper, 2)
 # 95% CI for ROI: -0.05% - 5.00%
+
+
+# necessary x_bar to break even
+x_even <- fixed_costs / (2*dentists_using*52)
+round(x_even, 3)
+# each dentist using Caridex needs to treat mean of 3.846 cavities per week for NPD to break even
+
